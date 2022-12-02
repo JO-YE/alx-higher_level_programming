0ce57e063493b4de18467ccc41353cc93b9ae508
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-12-02 00:03:56 +0000

    updat

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     b = 5
     from calculator_1 import add, sub, mul, div
     print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} + {} = {}".format(a, b, sub(a, b)))
-    print("{} + {} = {}".format(a, b, mul(a, b)))
-    print("{} + {} = {}".format(a, b, div(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     sum = 0
     a = range(len(argv) - 1)
     for i in a:
-      sum += int(argv[i + 1])
+        sum += int(argv[i + 1])
     print("{}".format(sum))
