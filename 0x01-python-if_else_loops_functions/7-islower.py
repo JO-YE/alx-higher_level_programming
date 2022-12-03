6afb9d77558229ff50f6740fc36b95d8ea9b5712
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-12-03 16:52:29 +0000

    updat

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""Function that checks for lowercasecharacters.""" 
+"""Function that checks for lowercasecharacters."""
+
+
 def islower(c):
     letter = 97
     letter2 = 122
 # is the letter within a to z(lower)
     if c >= chr(letter) and c <= chr(letter2):
-# if yes return true and false otherwise
+    # if yes return true and false otherwise
         return True
     else:
         return False
