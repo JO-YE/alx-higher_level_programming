4b82a4b70642f9d4a3f45e948bd10ad5b498ba61
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-12-03 16:50:50 +0000

    updat

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Print string in uppercase."""
 def uppercase(str):
-# check for letters in the string given
+    # check for letters in the string given
     for letter in str:
-# is any of the letter lowercase
+    # is any of the letter lowercase
         if letter >= chr(97) and letter <= chr(122):
-# If yes,take out 32 from the ascii table to change to upper
+    # If yes,take out 32 from the ascii table to change to upper
             letter = chr(ord(letter) - 32)
 # ord convert given letter to intergar
 # chr convert to character
