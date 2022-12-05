8c736e72e689299c6a6245c1154a9cc7cf1eedc0
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-12-05 21:48:48 +0000

    update

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,9 +7,7 @@ def new_in_list(my_list, idx, element):
         return(my_list)
     if idx > (len(my_list) -1):
         return(my_list)
-
     my_new_list = my_list[:]
-
     my_new_list[idx] = element
     return(my_new_list[:])
     return(my_list)
