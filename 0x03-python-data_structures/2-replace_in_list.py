8890e23447ce0670feee8fe6fd9f6aa66cec99ce
Racheal <ainaoluwadamilolaracheal@gmail.com>
2022-12-06 00:35:35 +0100

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """Function that replaces an element of a list at a specific position."""
-
-
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return(my_list)
@@ -11,6 +9,6 @@ def replace_in_list(my_list, idx, element):
     if idx > index:
         return(my_list)
 
-    my_list[idx] = element # assigning the new element for the chosen inde
+    my_list[idx] = element
     
-    return(my_list[:])
+    return(my_list)
