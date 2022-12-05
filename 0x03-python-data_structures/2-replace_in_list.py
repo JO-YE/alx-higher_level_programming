3f9203a35e54d5480e348fa1bbc89536480eb563
Racheal <ainaoluwadamilolaracheal@gmail.com>
2022-12-06 00:38:25 +0100

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0:
-        return(my_list)
-
+        return my_list
     index = (len(my_list) - 1)
-
     if idx > index:
-        return(my_list)
-
+        return my_list
     my_list[idx] = element
-    
-    return(my_list)
+    return my_list
