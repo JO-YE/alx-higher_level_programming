de31cdbef27eaf86f7ec97c4cea55520bcefc253
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-12-07 06:11:15 +0000

    add

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,9 +8,10 @@ i = len(sys.argv) - 1
 # this was done to get the result in the task
 if i == 0:
     print("{} {}.".format(i, 'argument'))
+elif i == i:
+    print("{} {}:".format(i, 'argument'))
 else:
-    print("{} {}:".format(i, 'arguments'))
-if i >= 1:
-  for a in range(i):
+    print("{} arguments::".format(i))
+for a in range(i):
     print("{}: {}".format(a + 1, sys.argv[a + 1]))
 # the plus one will make it print the desired result
--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,8 @@
 
 
 def no_c(my_string):
-    my_string[].remove(c)
-    return(my_string)
+    new_str = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new_str += i
+    return (new_str)
