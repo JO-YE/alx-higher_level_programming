cb82eae10e538c00663389876856070a6f4b41cb
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-12-07 07:08:53 +0000

    add

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in range(len(matrix)):
+        for column in range(len(matrix[row])):
+            if column != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[row][column]), end='')
+        print()
