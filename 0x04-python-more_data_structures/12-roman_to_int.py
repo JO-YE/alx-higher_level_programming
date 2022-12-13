63a730db1ba2665bda1f6d4f8f09982fba187164
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-12-13 18:22:44 +0000

    update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,9 +4,9 @@ def roman_to_int(roman_string):
         return 0
     else:
         roman_value = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50,\
-    'C' : 100, 'D' : 500, 'M' : 1000}
+                        'C' : 100, 'D' : 500, 'M' : 1000}
         special_cases = {'IV' : 4, 'IX' : 9, 'XL' : 40, 'XC' : 90,\
-    'CD' : 400, 'CM' : 900}
+                            'CD' : 400, 'CM' : 900}
         total = 0
         index = 0
         while index < len(roman_string):
