afdc1dac24c1bf2c69763c39d0640c34a17a67e2
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-12-13 15:14:15 +0000

    update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    
+    if not a_dictionary:
+        return None
+    else:
+        num = 0
+        string = ""
+        for value in a_dictionary.keys():
+            if a_dictionary[value] > num:
+                num = a_dictionary[value]
+                string = value
+        return string
