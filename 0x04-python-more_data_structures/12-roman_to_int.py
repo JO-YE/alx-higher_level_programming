839ec86f231214e827569c678acf17e0c88a50c5
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-12-13 18:14:47 +0000

    update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,10 @@ def roman_to_int(roman_string):
     if (type(roman_string) is not str) or (not roman_string):
         return 0
     else:
-        roman_value = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
-        special_cases = {'IV' : 4, 'IX' : 9, 'XL' : 40, 'XC' : 90, 'CD' : 400, 'CM' : 900}
+        roman_value = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50,
+ 'C' : 100, 'D' : 500, 'M' : 1000}
+        special_cases = {'IV' : 4, 'IX' : 9, 'XL' : 40, 'XC' : 90,
+ 'CD' : 400, 'CM' : 900}
         total = 0
         index = 0
         while index < len(roman_string):
