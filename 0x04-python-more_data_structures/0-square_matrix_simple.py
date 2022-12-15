d44641902d0a89f59cb156c9551b6c874bb4533c
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-12-15 16:59:39 +0000

    update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix[:]
     for i in range(len(new_matrix)):
-        new_matrix[i] = list(map(lambda x : x**2, new_matrix[i]))
+        new_matrix[i] = list(map(lambda x: x**2, new_matrix[i]))
     return new_matrix
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,7 @@ def roman_to_int(roman_string):
         roman_value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
                        'D': 500, 'M': 1000}
         special_cases = {'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90,
-                            'CD': 400, 'CM': 900}
+                        'CD': 400, 'CM': 900}
         total = 0
         index = 0
         while index < len(roman_string):
