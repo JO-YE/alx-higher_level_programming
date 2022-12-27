4ec290d2e341bc00844aec023577d99fa8cca82f
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-12-27 17:20:26 +0000

    update

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""Representing class Square."""
+
+
+class Square:
+    """Define a square using setter and getter."""
+    def __init__(self, size=0):
+        if (type(size) != int):
+            raise TypeError("size must be an integar")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """This method returns the square area of the object."""
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if (type(value) != int):
+            raise TypeError("size must be an integar")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
