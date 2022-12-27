120bf6e067c0b5fe750392d39ef926ba2576274f
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-12-27 16:22:52 +0000

    update

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Representing class Square."""
+
+
+class Square:
+    """Private attribute 'size' must be an integar.
+    Otherwise raise a TypeError.
+    If size is less than 0, raise a ValueError.
+
+    """
+    def __init__(self, size=0):
+        if (type(size) != int):
+            raise TypeError('size must be an integar')
+        elif (size < 0):
+                raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
