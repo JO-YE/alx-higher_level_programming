db830bc76a1e1ada5d294506a1d3326766824eae
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-12-27 17:03:07 +0000

    update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,7 +9,6 @@ class Square:
     If size is less than 0, raise a ValueError.
     """
     def __init__(self, size=0):
-        self.__size = size
         if (type(size) != int):
             raise TypeError("size must be an integar")
         elif size < 0:
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,7 +5,14 @@
 
 class Square:
     """Public instance method."""
-    def __init__(self, size):
-        self.__size = size
+    def __init__(self, size=0):
+        if (type(size) != int):
+            raise TypeError("size must be an integar")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
     def area(self):
-        return self.__size ** 2)
+        """This method returns the square area of the object."""
+        return (self.__size ** 2)
