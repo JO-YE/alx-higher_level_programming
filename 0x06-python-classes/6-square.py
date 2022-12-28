79f8f8c5195f37b9419c85e53686546a19a34be7
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-12-28 04:00:00 +0000

    commit

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -37,8 +37,7 @@ class Square:
         if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[0]) != int:
-            raise TypeError("position must be a tuple of 2 positive intege
-rs")
+            raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[1]) != int:
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 or value[1] < 0:
