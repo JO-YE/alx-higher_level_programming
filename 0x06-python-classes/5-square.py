8dc43f6aa8b14f25f2be680d8df6dabab2ce3f3d
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-12-28 02:53:04 +0000

    update

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,4 +1,4 @@
-usr/bin/python3
+#!/usr/bin/python3
 
 """Representing class Square."""
 
@@ -6,10 +6,33 @@ usr/bin/python3
 class Square:
     """Continuation from 4-...py in addition with a pub.obj method."""
     def __init__(self, size=0):
-        self.__size = size
-
+        self.size = size
+    @property
     def size(self):
+        """Property to retrive it."""
         return self.__size
-
+    @size.setter
     def size(self, value):
-        
+        """Property setter to set it."""
+        if not type(value) is int:
+            raise TypeError("size must be an integar")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Returns the current square area."""
+        return (self.size ** 2)
+
+    def my_print(self):
+        """ Method that prints a # square according
+        to the size value
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for k in range(self.__size):
+                    print("#", end='')
+                print()
