5702b1b6fd2508e70683b8aac15983f5335035a8
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-12-28 03:55:31 +0000

    update

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -26,9 +26,11 @@ class Square:
 
     @property
     def position(self):
+        """Property to retrive it."""
         return self.__position
 
     @position.setter
+    """Method that sets the position value of a square object."""
     def position(self, value):
         if not type(value) is tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
