23369e8f7b568c8e2edfd3f824dbb0ed7b7bec46
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-12-29 13:54:51 +0000

    commit

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -5,7 +5,7 @@
 
 class Node:
     """Private instance data and next_node."""
-    def __init__(self, data, next_node=Node):
+    def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
 
@@ -25,23 +25,22 @@ class Node:
 
     @next_node.setter
     def next_node(self, value):
-        if (!(next_node)) or next_node == Node:
-            self.__next_node = value
-        else:
+        if not type(value) is Node and value is not None:
             raise TypeError('next_node must be a Node object')
+        self.__next_node = value
 
 class SinglyLinkedList:
     def __str__(self):
-    rtn = ""
-    ptr = self.__head
+        rtn = ""
+        ptr = self.__head
 
-    while ptr is not None:
-        rtn += str(ptr.data)
-        if ptr.next_node is not None:
-            rtn += "\n"
-        ptr = ptr.next_node
-
-    return rtn
+        while ptr is not None:
+            rtn += str(ptr.data)
+            if ptr.next_node is not None:
+                rtn += "\n"
+            ptr = ptr.next_node
+        
+        return rtn
 
     def __init__(self):
         self.__head = None
