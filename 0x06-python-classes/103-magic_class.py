15aea78232178e036afcf95660d820441641dd07
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-12-29 15:40:16 +0000

    commit

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Class MagicClass for python Bytecode."""
+
+import math
+
+
+class MagicClass:
+    """Class that stores properties of a circumference."""
+    def __init__(self, radius=0):
+        if (type(radius) != int) and (type(radius) != float):
+            raise TypeError('radius must be a number')
+    self.__radius = radius
+
+    def area(self):
+        return ((self.__radius ** 2) * math.pi)
+
+    def circumference:
+        return (2 * math.pi * self.__radius)
