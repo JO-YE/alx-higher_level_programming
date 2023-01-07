9fa1878a428cdc324fe16e0b0bc258e44defb0e4
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-01-07 18:15:22 +0000

    update

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+'''This module consist of a fxn that print lines.'''
+
+
+def text_indentation(text):
+    '''Function that prints a text with 2 lines after , ? and :.'''
+    if type(text) != str:
+        raise TypeError('text must be a string')
+    i = 0
+    while i < len(text):
+        print(text[i], end='')
+        if text[i] in ['.', '?', ':']:
+            print()
+            print()
+            i += 1
+        i += 1
