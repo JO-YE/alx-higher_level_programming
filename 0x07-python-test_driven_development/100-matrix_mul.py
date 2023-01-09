26e4c7046dfbfa37bc1cbde62c413695c180c312
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-01-09 17:11:58 +0000

    update

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -25,7 +25,7 @@ def matrix_mul(m_a, m_b):
             if not isinstance(element, (int, float)):
                 raise TypeError('m_a should contain only integers or floats')
         if len(row) != len(m_a[0]):
-            raise TypeError('each row of m_a must be of the same')
+            raise TypeError('each row of m_a must be of the same size')
 
     for row in m_b:
         if not isinstance(row, list):
@@ -34,7 +34,7 @@ def matrix_mul(m_a, m_b):
             if type(element) != int and type(element) != float:
                 raise TypeError('m_b should contain only integers or floats')
         if len(row) != len(m_b[0]):
-            raise TypeError('each row of m_a must be of the same')
+            raise TypeError('each row of m_b must be of the same size')
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
