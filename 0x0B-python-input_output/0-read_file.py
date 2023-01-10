f1a574b2ae2322cd614c383b3d4d39af33193d5e
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-01-10 19:29:05 +0000

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,5 @@
 
 def read_file(filename=""):
     '''A function that reads a text file and prints it to stdout.'''
-    with open(filename, encoding='UTF8') as f:
-        read_data = f.read()
-        print(read_data)
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read())
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''Input/Output functions.'''
+
+
+def write_file(filename="", text=""):
+    '''Function that writes a string to a text file
+    and returns the number of chaaracters written.
+    '''
+    with open(filename, 'w', encoding='UTF8') as f:
+       return f.write(text)
