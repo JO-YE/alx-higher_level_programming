fb8705bc2e178749d241d82b7f140ba698852ea3
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-01-10 20:00:05 +0000

    update

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 '''Module contain fxn that convert python to JSON.'''
-
 import json
+
 
 def to_json_string(my_obj):
     '''A function that returns the JSON representation of an object.'''
