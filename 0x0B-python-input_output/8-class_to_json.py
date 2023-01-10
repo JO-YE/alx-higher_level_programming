4d1a00d9481bfe2d042845d1a09a31c231c91487
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-01-10 21:54:43 +0000

    update

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+'''Module contain fxn that returns a dictionary for json serialization.'''
+
+
+def class_to_json(obj):
+    '''Returns the dictionary description with simple data structure
+    (list, dictionary, string, integer and boolean) for
+    JSON serialization of an object.
+    '''
+    return (obj.__dict__)
