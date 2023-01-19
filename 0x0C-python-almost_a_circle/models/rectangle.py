79f8be027586dc02965f0f90f7f35fb58949e772
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-01-19 16:45:53 +0000

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,34 +14,42 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
+    @property
     def get_width(self):
         ''' width getter '''
         return self.__width
 
+    @width.setter
     def set_width(self):
         ''' width setter '''
         self.__width = width
 
+    @property
     def get_height(self):
         ''' height getter '''
         return self.__height
 
+    @height.setter
     def set_height(self):
         ''' height setter '''
         self.__height = height
 
+    @property
     def get_x(self):
         ''' x getter '''
         return self.__x
 
+    @x.setter
     def set_x(self):
         ''' x setter '''
         self.__x = x
 
+    @property
     def get_y(self):
         ''' y getter '''
         return self.__y
 
+    @y.setter
     def set_y(self):
         ''' y setter '''
         self.__y = y
