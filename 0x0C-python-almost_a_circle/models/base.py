13dfaf22a0987b72c4248292328cf641dfedc48a
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-01-21 08:51:20 +0000

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,8 +6,9 @@
 class Base:
     '''Defining Base class.'''
     __nb_objects = 0
+
     def __init__(self, id=None):
-        if id != None:
+        if id is not None:
             self.id = id
         else:
             Base. __nb_objects += 1
