94160c8883b4f1c6b38157b2e76fbf055fe24bf7
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-03-17 03:09:16 -0700

    update

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,12 @@
-#!/bin/usr/node
+#!/usr/bin/node
+let size = Number(process.argv[2]);
+let height = size;
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  while (height > 0) {
+  let square = 'X'.repeat(size);
+  console.log(square);
+  height--;
+    }
+}
