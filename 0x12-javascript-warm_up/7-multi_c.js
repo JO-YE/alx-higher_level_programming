aaf6c2127edb797fd256beb0b521bd8699831d2b
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-03-16 22:49:31 -0700

    update

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+let sta = 'C is fun';
+let x = Number(process.argv[2]);
+if (x === NaN) {
+  console.log('Missing number of occurrences');
+} else {
+  while (x > 0);
+    console.log('C is fun');
+    x--;
+}
