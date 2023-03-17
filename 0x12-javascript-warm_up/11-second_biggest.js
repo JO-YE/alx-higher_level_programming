4a4d4dbc80d10bba8424119b5a550fd720cd3033
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-03-17 04:54:15 -0700

    update

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let search = process.argv.lenght;
+if (search <= 2) {
+  console.log('0');
+} else if (search === 3) {
+    console.log('0');
+} else {
+    for (let i = 2; i < search; i++) {
+    
