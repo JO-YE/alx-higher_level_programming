507c5d220a106781d1b69637844a96b4f6db8e5a
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-03-20 02:03:32 +0100

    displaying values in a list where it matches the argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute('SELECT * FROM states WHERE name = %s', (sys.argv[4],))
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{}'",
+                .format(sys.argv[4]))
     state = cur.fetchall()
 
     for s in state:
