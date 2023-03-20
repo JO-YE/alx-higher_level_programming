a574061bc8a39fd5b76ee9ae7a1b018751d83bef
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-03-20 18:02:18 +0100

    worked on Inner join on this task

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,9 +9,9 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT a.id, a.name, b.name
-                FROM cities AS a
-                INNER JOIN states AS b
+    cur.execute("SELECT a.id, a.name, b.name\
+                FROM cities AS a\
+                INNER JOIN states AS b\
                 ON a.state_id = b.id")
     state = cur.fetchall()
 
