7c0cd093c9860121507be9fc144e92b240d4b8d5
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-03-20 17:03:22 +0100

    same with tast 2- but safe from SQL Injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name = %s OR 1=1",
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s",
                 (sys.argv[4],))
     state = cur.fetchall()
 
