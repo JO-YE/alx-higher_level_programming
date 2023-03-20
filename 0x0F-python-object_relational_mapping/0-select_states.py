93ed4b04da4e01f2c9a332414110b2e6a4cfb065
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-03-20 01:38:29 +0100

    worked on some pycostyle error

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,8 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3])
     cur = conn.cursor()
     cur.execute('SELECT * FROM states')
     state = cur.fetchall()
