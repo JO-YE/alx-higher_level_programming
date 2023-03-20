f849bcdac3d86499250fe85825d72fad8df2bb1a
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-03-20 01:59:17 +0100

    displaying values in a list where it matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+a script that takes in an argument and displays all values in the states
+ table of hbtn_0e_0_usa where name matches the argument.
+'''
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM states WHERE name = %s', (sys.argv[4],))
+    state = cur.fetchall()
+
+    for s in state:
+        print(s)
+
+    cur.close()
+    conn.close()
