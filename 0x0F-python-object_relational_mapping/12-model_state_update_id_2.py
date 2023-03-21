817ed82bd9abaea0197edd6db087715819146d7f
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-03-22 00:34:24 +0100

    changing a state name to another name

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+a python file that contains the class definition of a State
+and an instance Base = declarative_base()
+'''
+from sqlalchemy import create_engine
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_update = session.query(State).filter(State.id == 2).first()
+    state.name = 'New Mexico'
+    session.commit()
+
+    session.close()
