68800952efc9ac3b9433ef11aaeffb961a680e16
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-03-23 23:35:00 +0100

    creating a relationship btw model-state and model_city

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''
-    creates the State “California” with the City “San Francisco”
+    Creates the State “California” with the City “San Francisco”
     from the database
 '''
 from sqlalchemy import create_engine
