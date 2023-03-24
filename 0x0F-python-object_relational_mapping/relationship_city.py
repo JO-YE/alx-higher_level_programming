1323542a7f4115666d740077c519b7b5b1fe5a13
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-03-24 23:41:52 +0100

    correction

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -4,7 +4,6 @@ A copy from model_city
 """
 from sqlalchemy import Integer, Column, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base
 
 
 class City(Base):
