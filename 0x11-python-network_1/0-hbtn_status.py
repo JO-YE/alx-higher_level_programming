719ac55df56bb337c268024268db0c9879758238
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-04-04 23:45:36 +0100

    using urllib to interact with a web

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,8 @@ import urllib.request
 
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        content = response.read()
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
     print('Body response:')
     print('\t- type: {}'.format(type(content)))
     print(f'\t- content: {content}')
