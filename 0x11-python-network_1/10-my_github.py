52a21f71c6b3cc00a1a82481169b6ef1ae703802
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-04-06 11:09:03 +0100

    connecting to github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,6 +6,7 @@ import sys
 import requests
 from requests.auth import HTTPBasicAuth
 
+
 if __name__ == '__main__':
     url = 'https://api.github.com/user'
     req = requests.get(url, auth=HTTPBasicAuth(sys.argv.[1], sys.argv[2]))
