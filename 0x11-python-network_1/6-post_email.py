8eee3218e05fc73dc39ef8d89b925d5f147d8f86
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-04-06 14:51:04 +0100

    sending a POST request using requests package

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -8,7 +8,7 @@ import requests
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    data = {'email': sys.argv[2]}
+    datas = {'email': sys.argv[2]}
 
-    req = requests.post(url, data=data)
+    req = requests.post(url, data=datas)
     print(req.text)
