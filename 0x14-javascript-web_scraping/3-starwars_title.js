5a6db0eae0e02e93db6a3ca53198d0a429bc711a
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-05-04 15:22:44 +0100

    prints the number of movies where the character “Wedge Antilles” is present.

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
-// print title of a Star Wars movies
+// display the status of a GET request
 
 const request = require('request');
-const actor = 'https://swapi-api.alx-tools.com/api/people/18/'
-const url = process.argv[2];
+const id = process.agrv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/:id' + id;
 
-// => can also be used instead of the function keyword just like in task 0 & 1
 request(url, function (err, response, data) {
   if (err) {
     console.log(err);
   } else {
-    const films = JSON.parse(data).results;
-    const match = films.filter((film) => file.characters.includes(actor));
-    console.log(`${match.length}`);
+    console.log(JSON.parse(data).title);
   }
 });
