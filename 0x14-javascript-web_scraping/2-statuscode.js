0fe20d09091c01dac54582e59abd3d9e52e04e56
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-05-04 14:35:44 +0100

    display status code of a GET request

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,12 +4,10 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (err, response, data)
-  {
-    if (err) {
-      console.log(err);
-    }
-    else {
-      console.log(`code: ${response.statusCode}`);
-    }
-  });
+request(url, function (err, response, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`code: ${response.statusCode}`);
+  }
+});
