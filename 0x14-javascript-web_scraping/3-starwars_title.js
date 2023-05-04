d97511d45b4c963c76defabb31f24e23c83ae8cb
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2023-05-04 15:24:04 +0100

    prints the number of movies where the character “Wedge Antilles” is present.

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,7 @@ const request = require('request');
 const id = process.agrv[2];
 const url = 'https://swapi-api.alx-tools.com/api/films/:id' + id;
 
-request(url, function (err, response, data) {
+request.get(url, function (err, response, data) {
   if (err) {
     console.log(err);
   } else {
